935d88d4fa0d37687ab41f1315319408328b2fa7
Richard-tech-alt <richardhawk803@gmail.com>
2025-09-03 23:02:40 -0700

    dsc

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App(){
           <Route path="/midwest" element={<MidwestPage/>} />
           <Route path="/florida" element={<FloridaPage/>} />
           <Route path="/northwest" element={<NorthwestPage/>} />
-          <Route path="/southern" element={<SouthernPage/>} />
+          <Route path="/south" element={<SouthernPage/>} />
           <Route path="/southwest" element={<SouthwestPage/>} />
           <Route path="/east-coast" element={<EastCoastPage/>} />
         </Routes>
